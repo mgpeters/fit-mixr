3ac25e736e42b7032b39626a1f35a07afcfc1358
Matt Peters <matthew.george.peters@gmail.com>
2020-01-21 12:40:25 -0500

    got create to work on mongoDB

--- server/controllers/fitMixrController.js
+++ server/controllers/fitMixrController.js
@@ -12,7 +12,21 @@ fitMixrController.findData = (req, res, next) => {
 };
 
 fitMixrController.testPost = (req, res, next) => {
-  
-}
+  const testData = {
+    //date: new Date("<YYYY-mm-dd>"),
+    workoutLength: 56,
+    test: 'Test',
+  }
+  console.log('testPost hit');
+  models.WorkoutData.create(testData)
+    .then((response) => {
+      console.log('Inserted to DB!');
+      return next();
+    }).catch((error) => {
+      console.log('Test Post Error Occurred');
+      return next(error);
+    })
+};
+
 
 module.exports = fitMixrController;
