a837553554dbc4d3b7b6ea4b74530a0d95c98f29
Matt Peters <matthew.george.peters@gmail.com>
2020-01-22 10:39:51 -0500

    cleaned up prior comps

--- client/components/App.js
+++ client/components/App.js
@@ -1,44 +1,12 @@
 import React, { Component } from 'react';
-import Row from './Row';
-import GameList from './GameList';
-import Leaders from './Leaders';
-
-const gameStore = [];
 
 const fetchTest = fetch('https://wger.de/api/v2/exercise/?limit=100&status=2&language=2&category=13').then((results) => results.json()).then((result) => console.log(result)).catch((err) => console.log(err.message));
 
-function getInitialState() {
-  return {
-    rows: [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', ''],
-    ],
-    turn: 'X',
-    winner: undefined,
-    gameList: gameStore,
-  };
-}
-
-function checkWin(rows) {
-  const combos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-  ];
-
-  const flattened = rows.reduce((acc, row) => acc.concat(row), []);
-
-  return combos.find((combo) => (
-    flattened[combo[0]] !== ''
-    && flattened[combo[0]] === flattened[combo[1]]
-    && flattened[combo[1]] === flattened[combo[2]]
-  ));
+const shuffleArray = (array) => {  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 1/22/2020
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
 
 class App extends Component {
@@ -68,51 +36,9 @@ class App extends Component {
   }
 
   render() {
-    const {
-      rows, turn, winner, gameList,
-    } = this.state;
-    const { handleClick } = this;
-
-    const rowElements = rows.map((letters, i) => (
-      <Row key={i} row={i} letters={letters} handleClick={handleClick} />
-    ));
-
-    let infoDiv;
-    if (winner) {
-      const winTurn = turn === 'X' ? 'O' : 'X';
-      infoDiv = (
-        <div>
-          <div>
-Player
-            {' '}
-            {winTurn}
-            {' '}
-wins with squares
-            {' '}
-            {winner.join(', ')}
-!
-          </div>
-        </div>
-      );
-    } else {
-      infoDiv = (
-        <div>
-Turn:
-          {' '}
-          {turn}
-        </div>
-      );
-    }
-
     return (
       <div>
-        {infoDiv}
-        <div id="board">
-          {rowElements}
-        </div>
-        <button id="reset" onClick={() => this.setState(getInitialState())}>Reset board</button>
-        <GameList gameList={gameList} />
-        <Leaders />
+
       </div>
     );
   }
