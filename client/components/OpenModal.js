e0d6300f2ccad1c48af16b2106132c6c4af6ff53
Matt Peters <matthew.george.peters@gmail.com>
2020-01-22 14:46:26 -0500

    modal icons lined up and MINT

--- client/components/OpenModal.js
+++ client/components/OpenModal.js
@@ -3,6 +3,11 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
 import shoulders from '../assets/icons/shoulders.png';
+import legs from '../assets/icons/legs.png';
+import chest from '../assets/icons/chest.png';
+import back from '../assets/icons/back.png';
+import core from '../assets/icons/abs.png';
+import arms from '../assets/icons/arms.png';
 
 function OpenModal(props) {
   return (
@@ -22,8 +27,31 @@ function OpenModal(props) {
       <Modal.Body>
         <section className="modal-body">
           <h2 className="modal-body-text">What Are You Working Today?</h2>
-          <div>
-            <img src={shoulders}></img>
+          <div className="modal-body-selection">
+            <div>
+              <img src={shoulders}></img>
+              <p>Shoulders</p>
+            </div>
+            <div>
+              <img src={legs}></img>
+              <p>Legs</p>
+            </div>
+            <div>
+              <img src={chest}></img>
+              <p>Chest</p>
+            </div>
+            <div>
+              <img src={back}></img>
+              <p>Back</p>
+            </div>
+            <div>
+              <img src={core}></img>
+              <p>Core</p>
+            </div>
+            <div>
+              <img src={arms}></img>
+              <p>Arms</p>
+            </div>
           </div>
         </section>
       </Modal.Body>
