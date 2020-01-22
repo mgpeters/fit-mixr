4975748ee6b85547fc32a774986fe8e945f8c69c
Matt Peters <matthew.george.peters@gmail.com>
2020-01-22 16:51:46 -0500

    updated state with current workout array of objects

--- client/components/App.js
+++ client/components/App.js
@@ -9,10 +9,13 @@ import OpenModal from './OpenModal';
 // const fetchTest = fetch('https://wger.de/api/v2/exercise/?limit=100&status=2&language=2&category=13').then((results) => results.json()).then((result) => console.log(result)).catch((err) => console.log(err.message));
 
 const shuffleArray = (array) => {  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 1/22/2020
+    console.log('prior to shuffle', array);
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+  console.log('after shuffle', array);
+  return array;
 }
 
 class App extends Component {
@@ -55,20 +58,19 @@ class App extends Component {
     this.state.workoutTypeId.forEach((id) => {
       promiseArray.push(fetch(`https://wger.de/api/v2/exercise/?limit=100&status=2&language=2&category=${id}`)
         .then((results) => results.json())
-        .then((result) => result.results)
-        .catch((err) => console.log(err.message)))
+          .then((result) => result.results.slice(0, 5))
+            .catch((err) => console.log(err.message)))
     });
-
-    console.log('promiseArray', promiseArray)
 
     Promise.all(promiseArray)
       .then((result) => {
-        console.log('result', result)
-        // this.setState({
-        // modalShow: false,
-        // })
-      })
-      .catch((error) => {
+        const concatResults = result.reduce((acc, val) => [...acc, ...val]);
+
+        this.setState({
+          generatedWorkout: concatResults,
+          modalShow: false,
+        })
+      }).catch((error) => {
         console.log(error)
       })
   }
